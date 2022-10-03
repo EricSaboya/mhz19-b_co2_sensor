f0661e604a385092847d092dd1ea52b4cc288624
Eric Saboya <44400065+EricSaboya@users.noreply.github.com>
2022-10-03 17:56:31 +0100

    Updated functions and description

--- mhz19_co2/analyse.py
+++ mhz19_co2/analyse.py
@@ -2,23 +2,27 @@
 # ****************************************************************************
 # Author: Eric Saboya, Dept. of Geographical Science, University of Bristol
 # Contact: eric.saboya@bristol.ac.uk
-# Created: 2-Oct-2022
+# Created: 1-Oct-2022
 # ****************************************************************************
 # About
-# Analyse datalogger script
+# Functions for analysing and plotting CO2 data
 # ****************************************************************************
 import os
 import numpy as np
 import pandas as pd 
+import matplotlib.pyplot 
 
 
+# Analysis functions
 def read_co2_logfile(logfile):
+	""" Read in csv logfile
+
+	"""
 	ds = pd.read_csv(logfile)
 	time = pd.to_datetime(ds['time'])
-	co2 = ds['co2']
+	co2 = ds['CO2']
 	co2_err = ds['uncertainty']
 	return time, co2, co2_err
-
 
 def data_five_minute_ave(t_data, c_data):
 	""" Average 10s CO2 sensor data to five minute averaged data
@@ -38,7 +42,6 @@ def data_five_minute_ave(t_data, c_data):
 	for i in range(0, n_len-30, 30):
 		time_mid = ((t_data[i+30]-t_data[i])/2) + t_data[i]
 		time_5min.append(time_mid)
-
 		co2_mean = np.nanmean(c_data[i:i+30])
 		co2_5min_mean.append(co2_mean)
 		co2_stdev = np.std(c_data[i:i+30])
@@ -46,5 +49,47 @@ def data_five_minute_ave(t_data, c_data):
 
 	return np.array(time_5min), np.array(co2_5min_mean), np.array(co2_5min_stdev)
 
+def propagate_uncert(cstdev, c_uncert_mean):
+	""" propagate uncertainty values for five-min averaged data
+	
+	args:
+		cstdev: standard deviation of concentration measurements in 
+		        5-min period. 
+		c_uncert_mean: average uncertainty value from 5-min period
+
+	"""
+	return np.sqrt((cstdev**2)+(c_uncert_mean**2))
+
+
+# Plotting functions
+def plot_timeseries(xdata, ydata, yerr, xlabel, ylabel,  savefigure):
+	""" Plot time series data
+	 	args:
+	 		xdata (float): x-axis data (usually datetime data)
+	 		ydata (float): y-axis data 
+	 		yerr (float): y-axis data error values
+	 		xlabel (str): x-axis label
+	 		ylabel (str): y-axis label
+	 		savefigure (str): path+fname for figure saving
+
+	"""
+	fig, ax = plt.subplots(figsize=(10,8))
+	ax.errorbar(xdata, 
+		        ydata, 
+		        yerr=yerr, 
+		        fmt='o', 
+		        ecolor='k', 
+		        elinewidth=1.0, 
+		        markersize=7.0, 
+		        markerfacecolor='b', 
+		        markeredgecolor='k', 
+		        capsize=5, 
+		        capthick=1.0)
+
+	ax.set_xlabel(xlabel)
+	ax.set_ylabel(ylabel)
+	fig.tight_layout()
+	plt.savefig(savefigure, dpi=300)
+	plt.close()
 
 
